abd37ce3f34702d740155c9a6b99b4befa9697f7
shilo leopold <shilo.leopold@live.biu.ac.il>
2020-04-05 18:14:49 +0300

    5/4 18:16 DashbaordVM

--- FilghtSimulatorApp/Model/IModel.cs
+++ FilghtSimulatorApp/Model/IModel.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilghtSimulatorApp.Model
 {
-    interface IModel
+    interface IModel : INotifyPropertyChanged
     {
         void connect(string ip, int port);
         void disconnect();
         void start();
 
-        string IndicatedHeadingDeg {get;set;}
-        string GpsIndicatedVerticalSpeed {get;set;}
-        string GpsIndicatedGroundSpeedKt {get;set;}
+        string IndicatedHeadingDeg { get; set; }
+        string GpsIndicatedVerticalSpeed { get; set; }
+        string GpsIndicatedGroundSpeedKt { get; set; }
         string AirspeedIndicatorIndicatedSpeedKt { get; set; }
         string GpsIndicatedAltitudeFt { get; set; }
         string AttitudeIndicatorInternalRollDeg { get; set; }
         string AttitudeIndicatorInternalPitchDeg { get; set; }
-        string AltimeterIndicatedAltitudeFt {get;set;}
-}
+        string AltimeterIndicatedAltitudeFt { get; set; }
+    }
 }
--- FilghtSimulatorApp/Model/myModel.cs
+++ FilghtSimulatorApp/Model/myModel.cs
@@ -191,7 +191,6 @@ namespace FilghtSimulatorApp.Model
             }
         }
 
-
         public void NotifyPropertyChanged(string propName) {
             if (this.PropertyChanged != null)
             {
