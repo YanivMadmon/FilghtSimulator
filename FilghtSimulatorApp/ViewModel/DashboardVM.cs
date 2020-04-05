8d431a681551af662cbf6646c05c0294023130e4
Aviwer <aviwer613@gmail.com>
2020-04-05 16:47:22 +0300

    5.4.20 16:45

---
+++ FilghtSimulatorApp/ViewModel/DashboardVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class DashboardVM:INotifyPropertyChanged 
+{
+	private IModel model;
+	public event PropertyChangedEventHandler PropertyChanged;
+	public DashboardVM(IModel m)
+	{
+		this.model = m;
+        model.PropertyChanged +=
+        delegate (Object sender, PropertyChangedEventArgs e) {
+            NotifyPropertyChanged("VM_" + e.PropertyName);
+        };
+    }
+    public string VM_IndicatedHeadingDeg
+    {
+        get { return model.IndicatedHeadingDeg; }
+    }
+    public string VM_GpsIndicatedVerticalSpeed
+    {
+        get { return model.GpsIndicatedVerticalSpeed; }
+    }
+    public string VM_GpsIndicatedGroundSpeedKt
+    {
+        get { return model.GpsIndicatedGroundSpeedKt; }
+    }
+    public string VM_AirspeedIndicatorIndicatedSpeedKt
+    {
+        get { return model.AirspeedIndicatorIndicatedSpeedKt; }
+    }
+    public string VM_GpsIndicatedAltitudeFt
+    {
+        get { return model.GpsIndicatedAltitudeFt; }
+    }
+    public string VM_AttitudeIndicatorInternalRollDeg
+    {
+        get { return model.AttitudeIndicatorInternalRollDeg; }
+    }
+    public string VM_AttitudeIndicatorInternalPitchDeg
+    {
+        get { return model.AttitudeIndicatorInternalPitchDeg; }
+    }
+    public string VM_AltimeterIndicatedAltitudeFt
+    {
+        get { return model.AltimeterIndicatedAltitudeFt; }
+    }
+
+
+    public void NotifyPropertyChanged(string propName)
+    {
+        if (this.PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
+
+
